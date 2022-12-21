d9b7cea89f2a7f2a27f5635488ce7f8c1947ff8e
Amauri <hamauri@aol.com>
2022-12-20 21:14:35 -0800

    build basic html for homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,11 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-   
+   res.render('homepage', {
+    // just for implementations, change later
+    loggedIn: true,
+    userActive: true,
+   });
   } catch (err) {
     res.status(500).json(err);
   }
