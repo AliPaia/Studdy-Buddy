46124bd432c4fda4a0537149c29228c882828fa3
Amauri <hamauri@aol.com>
2022-12-22 20:46:17 -0800

    replaced missing code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,6 +14,17 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/assessment', async (req, res) => {
+  try {
+    res.render('assessment', {
+      // just for implementations, change later
+      loggedIn: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/profile');
