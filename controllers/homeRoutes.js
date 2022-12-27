e4ab8bf91629b39fd03e7f5e86c553a7e620672d
Alison Paia <alisonpaia@Alisons-MacBook-Pro.local>
2022-12-26 23:20:30 -0800

    login page initial html

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,16 +3,15 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-   res.render('homepage', {
-    // just for implementations, change later
-    loggedIn: true,
-    userActive: true,
-   });
+    res.render('homepage', {
+      // just for implementations, change later
+      loggedIn: true,
+      userActive: true,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
