e065d6380cff930f348fb496aa6f5d67b31209d6
Shayna Murphy <110637697+Shaynam1977@users.noreply.github.com>
2022-12-29 19:09:49 -0800

    handlebar tab

---
+++ public/js/logout.js
@@ -0,0 +1,29 @@
+// + new ActionRowBuilder<ButtonBuilder>()
+
+// const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Events } = require('discord.js');
+
+// client.on(Events.InteractionCreate, async interaction => {
+// 	if (!interaction.isChatInputCommand()) return;
+
+// 	if (interaction.commandName === 'button') {
+// 		const row = new ActionRowBuilder()
+// 			.addComponents(
+// 				// ...
+// 			);
+
+// 		const embed = new EmbedBuilder()
+// 			.setColor(0x0099FF)
+// 			.setTitle('Some title')
+// 			.setURL('https://discord.js.org')
+// 			.setDescription('Some description here');
+
+// 		await interaction.reply({ content: 'I think you should,', ephemeral: true, embeds: [embed], components: [row] });
+// 	}
+// });
+
+// client.on(Events.InteractionCreate, interaction => {
+// 	if (!interaction.isButton()) return;
+// 	console.log(interaction);
+// });
+
+console.log("we made it!!!!")
