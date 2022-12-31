6165cd3f0f8708e19860e8987a1d9de705786404
Amauri <hamauri@aol.com>
2022-12-30 16:35:32 -0800

    ran prettier

--- public/js/chat.js
+++ public/js/chat.js
@@ -29,7 +29,7 @@ const sendMessage = (event) => {
 
 const addMessage = (message, user) => {
   const li = document.createElement('li');
-  li.classList.add(user)
+  li.classList.add(user);
   li.innerHTML = message;
   messagesEl.appendChild(li);
   li.scrollIntoView();
