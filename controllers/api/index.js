302a0cab55e0966a6dd902936377384b1af0df0b
Hailey Loechler <haileyloechler@gmail.com>
2023-01-02 17:27:41 -0800

    added scores route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const chatRoutes = require('./chatRoutes');
+const scoreRoutes = require('./scoreRoutes');
 
 router.use('/users', userRoutes);
 router.use('/chats', chatRoutes);
+router.use('/scores', scoreRoutes);
 
 module.exports = router;
